b88f1c2967700dd7f294d5e3db6acbe4cab4ebe4
李鹏 <lipeng@hiveview.com>
2015-04-30 14:42:53 +0800

    添加csp

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,22 @@ router.get('/', function (req, res) {
   if (Number(uajson.VERSION) < 9 && uajson.IE) {
     res.send("由于使用websocket和css3故暂不支持IE浏览器，如使用双核浏览器请切换至webkit内核。");
   } else {
+    var csp = "default-src 'self'; " +
+              "script-src 'self'; " +
+              "object-src 'none'; " +
+              "img-src 'self'; " +
+              "media-src 'none'; " +
+              "frame-src 'none'; " +
+              "font-src 'none'; " +
+              "connect-src 'self';";
+
     res.render('index.jade', { title : 'User', user : user });
+    res.setHeader("Content-Security-Policy", csp);
+    res.setHeader("X-Content-Security-Policy", csp);
+    res.setHeader("X-WebKit-CSP", csp);
+    res.setHeader("Expires", "Mon, 20 Jul 2009 23:00:00 GMT");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Pragma", "no-cache");
   }
 });
 
